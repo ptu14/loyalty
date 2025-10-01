00f4b574146d50d681e3d8156b6c2c9715e15584
Mateusz <ptu@vp.pl>
2025-10-01 16:49:44 +0200

    dodanie open graph

--- app/page.js
+++ app/page.js
@@ -5,6 +5,22 @@ export const metadata = {
   icons: {
     icon: 'assets/images/loyster.svg',
   },
+  openGraph: {
+    title: "Loyster - Profesjonalne aplikacje lojalnościowe dla Twojego biznesu",
+    description: "Loyster to kompleksowa platforma do tworzenia i zarządzania programami lojalnościowymi, która pomoże Ci zwiększyć zaangażowanie klientów i rozwinąć biznes.",
+    url: 'https://www.loyster.pl/', // Zamień na swój URL
+    siteName: 'Loyster',
+    images: [
+      {
+        url: '/assets/images/logo.svg',
+        width: 117,
+        height: 40,
+        alt: 'Logo Loyster',
+      }
+    ],
+    locale: 'pl_PL',
+    type: 'website',
+  },
 };
 export default function HomePage() {
   return (
